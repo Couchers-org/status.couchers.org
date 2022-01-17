a9bc3c85c34bcaff3ea048932a73414ef952d8cc
Aapeli <git@aapelivuorinen.com>
2022-01-17 13:12:54 +0200

    Add last updated time

--- build.py
+++ build.py
@@ -1,9 +1,34 @@
 #!/bin/python3
 
 import shutil
+from datetime import datetime
 from pathlib import Path
+from subprocess import check_output
 
 print("Building status page...")
+
+# prints out unix timestamp of last modified
+last_updated_raw = (
+    check_output(
+        [
+            "git",
+            "log",
+            "--date-order",
+            "--quiet",
+            "--format=tformat:%ct",
+            "--max-count=1",
+            "--",
+            "./status",
+        ]
+    )
+    .decode("utf8")
+    .strip()
+)
+
+last_updated = datetime.fromtimestamp(int(last_updated_raw))
+
+last_updated_display = f"{last_updated.isoformat()} UTC"
+last_updated_timestamp = int(last_updated.timestamp())
 
 with open("status") as f:
     data = [line.strip() for line in f.readlines() if line[0] != "#"]
@@ -20,14 +45,18 @@ print("Current status:")
 print(f"State: {state}")
 print(f"Title: {title}")
 print(f"Description: {description}")
+print(f"last_updated_display: {last_updated_display}")
+print(f"last_updated_timestamp: {last_updated_timestamp}")
 
 
 def replace(doc):
     return (
         doc
         .replace("{{state}}", state)
-        .replace("{{description}}", description)
         .replace("{{title}}", title)
+        .replace("{{description}}", description)
+        .replace("{{last_updated_display}}", str(last_updated_display))
+        .replace("{{last_updated_timestamp}}", str(last_updated_timestamp))
     )
 
 
